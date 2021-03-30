b846ac8ecd3920e44ca41734c2911dce30636c19
tykatyk <tykatyk@gmail.com>
2021-03-30 12:17:23 +0300

    Added view for desktop version of a payment page

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get("/payment", function(){
+  return view("paymentPage.paymentForm");
+});
+
+Route::post("/payment", function(){
+  return view("paymentPage.paymentForm");
+});
