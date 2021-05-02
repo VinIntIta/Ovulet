b0acf712cc82d54dfdf6b32de34a41555c4a5701
tykatyk <tykatyk@gmail.com>
2021-05-02 13:00:18 +0300

    Modified head.blade to enable showing correct title

--- resources/views/calendarPage/calendarPage.blade.php
+++ resources/views/calendarPage/calendarPage.blade.php
@@ -1,9 +1,21 @@
 @php
-$warningMsg = isset($warningMsg) ? $warningMsg : null
+  $warningMsg = isset($warningMsg) ? $warningMsg : null;
 @endphp
 
-@if(@isset($isMobile))
-  <x-calendar.calendarPageMobile :warningMsg="$warningMsg"/>
-@else
-  <x-calendar.calendarPageDesktop :warningMsg="$warningMsg"/>
-@endif
+<!DOCTYPE html>
+<html lang="en">
+  <x-layouts.head>
+    <x-slot name="title">Calendar</x-slot>
+  </x-layouts.head>
+  <body>
+    <x-layouts.header/>
+  	<div>
+      @if(@isset($isMobile))
+        <x-calendar.calendarPageMobile :warningMsg="$warningMsg"/>
+      @else
+        <x-calendar.calendarPageDesktop :warningMsg="$warningMsg"/>
+      @endif
+    </div>
+    <x-layouts.footer/>
+  </body>
+</html>
